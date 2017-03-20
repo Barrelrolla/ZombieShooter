a54f360c6a962c4c19ae98be58d80545adb2f6f3
JT <barrelrolla@JTs-Mac.local>
2017-03-21 01:43:26 +0200

    Implemented shooting, but needs a lot more work

---
+++ ZombieShooter/Bullet.swift
@@ -0,0 +1,21 @@
+//
+//  Bullet.swift
+//  ZombieShooter
+//
+//  Created by JT on 3/20/17.
+//  Copyright © 2017 JT. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Bullet : SKSpriteNode {
+    
+    override init(texture: SKTexture!, color: SKColor, size: CGSize) {
+        super.init(texture: texture, color: color, size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
