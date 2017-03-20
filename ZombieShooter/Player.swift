6d234d4d2b05e34e214409d7a659ce5d01088259
JT <barrelrolla@JTs-Mac.local>
2017-03-20 16:21:27 +0200

    Implemented joysticks

---
+++ ZombieShooter/Player.swift
@@ -0,0 +1,23 @@
+//
+//  Player.swift
+//  ZombieShooter
+//
+//  Created by JT on 3/20/17.
+//  Copyright © 2017 JT. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Player : SKSpriteNode {
+    private var ammo: Int = 100
+    private var weapon: Int = 1
+    
+    override init(texture: SKTexture!, color: SKColor, size: CGSize) {
+        super.init(texture: texture, color: color, size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
