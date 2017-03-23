e5eaf9adcf3ecfcb9d5d91d0dca4053cbcc4eb73
JT <chetkara@gmail.com>
2017-03-24 01:59:01 +0200

    fix the repo

---
+++ ZombieShooter/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  ZombieShooter
+//
+//  Created by JT on 3/21/17.
+//  Copyright © 2017 JT. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    static let FontName = "FeastofFleshBB"
+}
